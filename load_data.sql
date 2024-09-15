bd81ae946a505f536fd0c8a9c881f68fcd366220
Mihir Gajjar <119708204+themihirgajjar@users.noreply.github.com>
2024-09-15 16:24:52 +0100

    Update load_data.sql

--- load_data.sql
+++ load_data.sql
@@ -1,35 +1,35 @@
 -- stations
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/station_names.csv stations
+.import --csv --skip 1 /bcur_csvs/station_names.csv stations
 
 -- coordinates
-.import --csv --skip 1 /Users/mihirgajjar/Desktop/python/bcur_csvs/coordinates_by_id.csv coordinates
+.import --csv --skip 1 /bcur_csvs/coordinates_by_id.csv coordinates
 
 -- 2019_friday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2019_friday.csv friday_2019
+.import --csv --skip 1 /bcur_csvs/2019_friday.csv friday_2019
 
 -- 2019_montothu
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2019_montothu.csv montothu_2019
+.import --csv --skip 1 /bcur_csvs/2019_montothu.csv montothu_2019
 
 -- 2019_saturday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2019_saturday.csv saturday_2019
+.import --csv --skip 1 /bcur_csvs/2019_saturday.csv saturday_2019
 
 -- 2019_sunday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2019_sunday.csv sunday_2019
+.import --csv --skip 1 /bcur_csvs/2019_sunday.csv sunday_2019
 
 -- 2022_friday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2022_friday.csv friday_2022
+.import --csv --skip 1 /bcur_csvs/2022_friday.csv friday_2022
 
 -- 2022_monday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2022_monday.csv monday_2022
+.import --csv --skip 1 /bcur_csvs/2022_monday.csv monday_2022
 
 -- 2022_saturday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2022_saturday.csv saturday_2022
+.import --csv --skip 1 /bcur_csvs/2022_saturday.csv saturday_2022
 
 -- 2022_sunday
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2022_sunday.csv sunday_2022
+.import --csv --skip 1 /bcur_csvs/2022_sunday.csv sunday_2022
 
 -- 2022_tuetothu
-.import --csv --skip 1 /workspaces/119708204/bcur_csvs/2022_tuetothu.csv tuetothu_2022
+.import --csv --skip 1 /bcur_csvs/2022_tuetothu.csv tuetothu_2022
 
 
 -- morning	05:00-07:00
